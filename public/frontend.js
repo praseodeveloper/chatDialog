811fec9667c77b271c190bf6f387cc401bdb73e0
Pradeep Balagangadharan <praseodeveloper@gmail.com>
2024-03-10 01:58:39 +0100

    Switch to web component

--- public/frontend.js
+++ public/frontend.js
@@ -1,20 +1,7 @@
-const fqdn = "http://localhost:3000";
-
-function createFragmentFromHTML(htmlString) {
-    const range = document.createRange();
-    range.selectNode(document.body); // Select any existing node as the context
-    const fragment = range.createContextualFragment(htmlString);
-    return fragment;
-}
-
 document.addEventListener("DOMContentLoaded", function () {
-    const chatbox = document.querySelector(".chatbox");
-    const messageContainer = document.querySelector(".card-body");
+    const chatbox = document.querySelector("#myChat");
     const overlay = document.querySelector(".overlay");
     const openChatboxBtn = document.querySelector("#openBtn");
-    const closeChatboxBtn = document.querySelector("#btn-close");
-    const messageInput = document.querySelector("#input");
-    const submitBtn = document.querySelector('#submit');
 
     const openChatbox = function () {
         chatbox.classList.remove("hidden");
@@ -28,96 +15,13 @@ document.addEventListener("DOMContentLoaded", function () {
         overlay.classList.add("hidden");
     };
 
-    closeChatboxBtn.addEventListener("click", closeChatbox);
+    chatbox.addEventListener("chatbox-closed-event", closeChatbox);
     overlay.addEventListener("click", closeChatbox);
 
     document.addEventListener("keydown", function (e) {
         if (e.key === "Escape" && !chatbox.classList.contains("hidden")) {
             closeChatbox();
         }
-    });
-
-    const addMessage = function (sender, text) {
-        if (!text) {
-            return;
-        }
-
-        let newMessage;
-        if (sender === "user") {
-            newMessage = `
-                <div class="d-flex align-items-baseline text-end justify-content-end mb-4">
-                    <div class="pe-2">
-                        <div>
-                            <div class="card card-text d-inline-block p-2 px-3 m-1">
-                                ${text}
-                            </div>
-                        </div>
-                    </div>
-                    <div class="position-relative avatar">
-                        <img src="user.png" class="img-fluid rounded-circle" alt="" />
-                    </div>
-                </div>
-            `;
-        } else if (sender === "bot") {
-            newMessage = `
-                <div class="d-flex align-items-baseline mb-4">
-                    <div class="position-relative avatar">
-                        <img src="assistant.png" class="img-fluid rounded-circle" alt="" />
-                    </div>
-                    <div class="pe-2">
-                        <div>
-                            <div class="card card-text d-inline-block p-2 px-3 m-1">
-                                ${text}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            `;
-        }
-
-        const messageElement = createFragmentFromHTML(newMessage);
-        messageContainer.appendChild(messageElement);
-        messageContainer.scrollTop = messageContainer.scrollHeight;
-    }
-
-    const sendPrompt = function (msg) {
-        return fetch(`${fqdn}/sendPrompt`, {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8'
-            },
-            body: JSON.stringify({
-                prompt: msg
-            })
-        }).then((response) => response.json()). //
-            catch((err) => { throw new Error(err); });
-    }
-
-    const processInput = function () {
-        const busyIndicator = document.querySelector(".busy-indicator");
-        const msg = messageInput.value;
-        addMessage("user", msg);
-        messageInput.value = "";
-        busyIndicator.classList.remove("hidden");
-        sendPrompt(msg). //
-            then((reply) => {
-                busyIndicator.classList.add("hidden");
-                addMessage("bot", reply.response);
-            }). //
-            catch((ex) => {
-                busyIndicator.classList.add("hidden");
-                addMessage("bot", `An error occurred : ${ex}`)
-            });
-    }
-
-    messageInput.addEventListener("keyup", (event) => {
-        if (event.key === "Enter") {
-            processInput();
-        }
-    });
-
-    submitBtn.addEventListener("click", () => {
-        processInput();
     });
 });
 
---
+++ public/webc/chatbox.js
@@ -0,0 +1,240 @@
+class ChatBox extends HTMLElement {
+    static observedAttributes = ["width", "height"];
+
+    constructor() {
+        super();
+        
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
+            <style>
+                .card.mx-auto {
+                    height: 600px;
+                    width: 600px;
+                }
+
+                a.nav-link {
+                    color: gray;
+                    font-size: 18px;
+                    padding: 0;
+                }
+                
+                .avatar {
+                    width: 50px;
+                    height: 50px;
+                    border-radius: 50%;
+                    border: 2px solid #cf5479;
+                    padding: 2px;
+                    flex: none;
+                }
+                
+                input:focus {
+                    outline: 0px !important;
+                    box-shadow: none !important;
+                }
+
+                .card-body {
+                    overflow: auto;
+                }
+                
+                .card-text {
+                    border: 2px solid #91cdc9;
+                    border-radius: 4px;
+                    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+                    font-size: 14px;
+                    max-width: 500px;
+                }
+                
+                .btn {
+                    display: inline-block;
+                    padding: 0.8rem 1.4rem;
+                    font-weight: 700;
+                    background-color: black;
+                    color: white;
+                    border-radius: 5px;
+                    text-align: center;
+                    font-size: 1em;
+                }
+                
+                .busy-indicator {
+                    justify-content: center;
+                    display: flex;
+                    flex: auto;
+                    padding: 1rem;
+                }
+                
+                .hidden {
+                    display: none;
+                }
+
+                .card-pre {
+                    white-space: pre-wrap;
+                    word-wrap: break-word;
+                }
+
+                .card-pre code {
+                    font-size : 14px;
+                }
+            </style>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/css/bootstrap.min.css" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+            
+            <div class="card mx-auto">
+    
+                <div class="card-header">
+                    <div class="navbar navbar-expand p-0">
+                        <ul class="navbar-nav me-auto align-items-center">
+                            <li class="nav-item">
+                                <a href="#!" class="nav-link text-dark">Piloting AI Assistant</a>
+                            </li>
+                        </ul>
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <a href="#!" class="nav-link">
+                                    <i id="btn-close" class="fas fa-window-minimize"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+    
+                <div class="card-body p-3">
+                </div>
+    
+                <div class="busy-indicator hidden">
+                    <i class="fas fa-spinner fa-pulse"></i>
+                </div>
+    
+                <div class="card-footer w-100 bottom-0 m-0 p-1">
+                    <div class="input-group border-1">
+                        <input type="text" class="form-control" id="input" placeholder="Enter your text here..." />
+                        <div class="input-group-text bg-transparent">
+                            <button id="submit" class="btn btn-light text-secondary">
+                                <i class="fas fa-paper-plane"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        // Event handlers
+        const messageInput = this.shadowRoot.querySelector("#input");
+        messageInput.addEventListener("keyup", (event) => {
+            if (event.key === "Enter") {
+                this.processInput();
+            }
+        });
+
+        const submitBtn = this.shadowRoot.querySelector("#submit");
+        submitBtn.addEventListener("click", () => {
+            this.processInput();
+        });
+
+        const closeChatboxBtn = this.shadowRoot.querySelector("#btn-close");
+        closeChatboxBtn.addEventListener("click", this.closeChatbox.bind(this));
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log(
+          `Attribute ${name} has changed from ${oldValue} to ${newValue}.`,
+        );
+        const box = this.shadowRoot.querySelector(".card.mx-auto");
+        const msgCards = this.shadowRoot.querySelectorAll(".card-text");
+        if(name === "height") {
+            box.style.height = newValue;
+        } else if(name === "width") {
+            box.style.width = newValue;
+            msgCards.forEach(msgCard => { 
+                msgCard.style.maxWidth = `calc(${newValue} - 100px)`; 
+            });
+        }
+      }
+
+    closeChatbox() {
+        this.classList.add("hidden");
+        const customEvent = new CustomEvent('chatbox-closed-event', {
+            bubbles: true, // Allow event to bubble up the DOM tree
+            composed: true, // Allow event to cross shadow DOM boundaries
+            detail: { id: this.id }
+          });
+        this.shadowRoot.dispatchEvent(customEvent);
+    };
+
+    processInput() {
+        const messageInput = this.shadowRoot.querySelector("#input");
+        const busyIndicator = this.shadowRoot.querySelector(".busy-indicator");
+        const msg = messageInput.value;
+        if (msg) {
+            this.addMessage("user", msg);
+            messageInput.value = "";
+            busyIndicator.classList.remove("hidden");
+            this.sendPrompt(msg). //
+                then((reply) => {
+                    busyIndicator.classList.add("hidden");
+                    this.addMessage("bot", reply.response);
+                }). //
+                catch((ex) => {
+                    busyIndicator.classList.add("hidden");
+                    this.addMessage("bot", `An error occurred : ${ex}`);
+                });
+        }
+    }
+
+    addMessage(sender, text) {
+        let newMessage;
+        if (sender === "user") {
+            newMessage = `
+                <div class="d-flex align-items-baseline text-end justify-content-end mb-4">
+                    <div class="pe-2">
+                        <div>
+                            <div class="card card-text d-inline-block p-2 px-3 m-1">
+                                <pre class="card-pre"><code>${text}</code></pre>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="position-relative avatar">
+                        <img src="webc/img/user.png" class="img-fluid rounded-circle" alt="" />
+                    </div>
+                </div>
+            `;
+        } else if (sender === "bot") {
+            newMessage = `
+                <div class="d-flex align-items-baseline mb-4">
+                    <div class="position-relative avatar">
+                        <img src="webc/img/assistant.png" class="img-fluid rounded-circle" alt="" />
+                    </div>
+                    <div class="pe-2">
+                        <div>
+                            <div class="card card-text d-inline-block p-2 px-3 m-1">
+                                <pre class="card-pre"><code>${text}</code></pre>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+        }
+
+        const messageElement = document.createElement("div");
+        messageElement.innerHTML = newMessage;
+        const messageContainer = this.shadowRoot.querySelector(".card-body");
+        messageContainer.appendChild(messageElement);
+        messageContainer.scrollTop = messageContainer.scrollHeight;
+    }
+
+    sendPrompt(msg) {
+        const fqdn = this.getAttribute("endpoint");
+        return fetch(`${fqdn}/sendPrompt`, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            },
+            body: JSON.stringify({
+                prompt: msg
+            })
+        }).then((response) => response.json()). //
+            catch((err) => { throw new Error(err); });
+    }
+
+}
+
+customElements.define('chat-box', ChatBox);
